67ceb86a52d4e3b835730b8b34618d1cb7bec7ef
Arun Nair <ariesaru2002@gmail.com>
2014-10-06 18:16:53 -0400

    Get environment variables

--- server1.js
+++ server1.js
@@ -1,11 +1,19 @@
 
 var express  = require('express');
+var mongojs = require('mongojs');
 var app = express();
 
 
 var ip = process.env.OPENSHIFT_NODEJS_IP;
 var port = process.env.OPENSHIFT_NODEJS_PORT || 4000;
+var db = mongojs("cs56101", ['db1']);
 
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/env', function(req, res) {
+	res.json(process.env);
+})
 app.get('/hello', function(req, res) {
 	res.send("Hello World!");
 })
