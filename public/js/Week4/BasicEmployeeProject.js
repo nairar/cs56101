636e34900c71d9843b63dbf4c49fe95fb5845056
Arun Nair <ariesaru2002@gmail.com>
2014-10-06 18:43:44 -0400

    Add basic html employee website using a get request

---
+++ public/js/Week4/BasicEmployeeProject.js
@@ -0,0 +1,9 @@
+﻿
+function TableController($scope, $http) {
+    var url = '/employees'
+    $http.get(url).success(function (res, err) {
+        $scope.employees = res;
+    });
+
+
+};
--- server1.js
+++ server1.js
@@ -1,22 +1,41 @@
 
 var express  = require('express');
 var mongojs = require('mongojs');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser());
 
 var ip = process.env.OPENSHIFT_NODEJS_IP;
 var port = process.env.OPENSHIFT_NODEJS_PORT || 4000;
-var db = mongojs("cs56101", ['db1']);
 
-app.use(express.static(__dirname + '/public'));
+var connectionString = "mongodb://admin:ieJrVhrTNTaL@127.5.80.2:27017/" + process.env.OPENSHIFT_APP_NAME;
 
+if (typeof process.env.OPENSHIFT_MONGODB_DB_URL == undefined) {
+	connectionString = "localhost:27018/cs56101";
+}
+var db = mongojs(connectionString, ['db1']);
 
 app.get('/env', function(req, res) {
 	res.json(process.env);
-})
+});
+
 app.get('/hello', function(req, res) {
 	res.send("Hello World!");
-})
+});
+
+app.get('/employees', function(req, res) {
+	res.sendfile('./public/BasicEmployeeProject.html');
+	
+});
+
+
+
+
+
+
+
 
 app.listen(port, ip, function() {
 	console.log("Server is listening on port "+ port);
